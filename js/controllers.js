b97ec6de5abdbc544757cea0b5ded64af5f10e3a
Hack Reactor Students <github@hackreactor.com>
2013-09-04 15:43:53 -0700

    controller makes ajax request to reddit and pulls controversial posts

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,11 @@
-angular.module('myApp.controllers', []);
+angular.module('myApp.controllers', [])
+.controller('MainController', ['$scope', '$http', function($scope, $http){
+  $scope.name = "Brett";
+  $http({
+    method: 'JSONP',
+    url: 'http://www.reddit.com/r/controversial.json?jsonp=JSON_CALLBACK'
+  }).success(function(data, status, headers, config) {
+    $scope.redditStuff = data.data.children;
+  });
+}]);
+
